0910ebdbf256102daabe969a7b7585e27a113072
刘晓明 <liuxiaoming1109@sina.com>
2019-06-14 11:20:23 +0800

    工程提交

---
+++ src/main/java/com/evan/SpringbootRedisShiroJwtApplication.java
@@ -0,0 +1,13 @@
+package com.evan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRedisShiroJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRedisShiroJwtApplication.class, args);
+	}
+
+}
